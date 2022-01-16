6e6d09f3d0ae9b783268bef4d1a4c9006ea327bb
Roman Kolivashko <rkolivashko@gmail.com>
2022-01-16 10:26:05 -0800

    Added Users component

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+const About = () => {
+  return ( 
+    <div>
+      <h1>About</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo sint aspernatur culpa eveniet neque quia? Molestias, recusandae minus architecto maxime repudiandae repellendus nulla nostrum necessitatibus, earum quasi veniam placeat libero.</p>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis alias tempora atque itaque molestias asperiores, culpa voluptas, repellat maiores iste doloremque distinctio in, reiciendis quod beatae aspernatur est exercitationem? Magnam.</p>
+    </div>
+   );
+}
+ 
+export default About;
